5e210777af607fecc82e5efd4ba83fb24a989a19
Sergei Kadushkin <blackoverlord.inc@mail.ru>
2023-09-06 13:42:10 +0300

    init com

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -8,7 +8,7 @@ public class Egg6 {
     private final Needle7 needle7;
 
     @Autowired
-    public Egg6(Needle7 needle7) {
+    public Egg6(Needle7 needle7) { //DI через конструктор
         this.needle7 = needle7;
     }
 
